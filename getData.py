6138879a6087df59ebc2880bc78ef94b518e1d30
panxiuqing <panxiuqing@gmail.com>
2015-05-19 06:17:35 +0800

    add timeout to avoid long time waiting

--- getData.py
+++ getData.py
@@ -1,12 +1,19 @@
+#!/usr/bin/python
+
 __author__ = 'pxqing'
 
+import time, random
 import urllib2 as u
+
 for H, E in [(6, 'ss'), (0, 'sz'), (3, 'sz')]:
     for i in range(0, 3000):
-        C = "%d%05d" % (H, i)
+        time.sleep(random.random()*2)
+	C = "%d%05d" % (H, i)
+	print "getting", C
         try:
-            f = u.urlopen('http://table.finance.yahoo.com/table.csv?s=' + C + '.' + E)
+	    f = u.urlopen('http://table.finance.yahoo.com/table.csv?s=' + C + '.' + E, timeout=5)
+	    print "writing", C
             with open('./Data/%s/%s.csv' % (E, C), 'w') as tb:
                 tb.write(f.read())
-        except u.HTTPError:
-            print "Not find %s" %C
+        except Exception, e:
+            print "get", C, e
